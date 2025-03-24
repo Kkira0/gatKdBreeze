9e566b16bca29c9fec19ef515e6d6a79958ad4be
pipisa <your@email.ex>
2025-03-24 10:55:30 +0200

    delete

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
     Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
     Route::put('/posts/{post}/update', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('/posts', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
     Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
 });
